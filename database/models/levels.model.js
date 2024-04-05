d9cf4c0260c9a5d53654b1576bf65b0fae9dc6ec
donia <doniafarag390@gmail.com>
2024-04-05 17:17:45 +0200

    hidethings

--- database/models/levels.model.js
+++ database/models/levels.model.js
@@ -1,7 +1,7 @@
 import mongoose, { Schema, model } from 'mongoose';
 const levelSchema=new Schema({
-   subjectId:{type:Schema.Types.ObjectId,ref:"subject",select:"false"},
-   subjectName:{type:String,select:"false"},
+   subjectId:{type:Schema.Types.ObjectId,ref:"subject",select:false},
+   subjectName:{type:String,select:false},
    levelName:{type:String,required:true},
    image:{
       public_id:String , secure_url:String
