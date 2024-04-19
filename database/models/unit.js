a323e8592c9cd3a558bd2fbd0d0943ae39495ce1
donia <doniafarag390@gmail.com>
2024-04-19 16:56:30 +0200

    hg

--- database/models/unit.js
+++ database/models/unit.js
@@ -1,9 +1,9 @@
 import mongoose, { Schema, model } from 'mongoose';
 const unitSchema=new Schema({
-   subjectId:{type:Schema.Types.ObjectId,ref:"subject"},
-   subjectName:{type:String,required:true},
-   levelName:{type:String,required:true},
-   levelId:{type:Schema.Types.ObjectId,ref:"Level"},
+   subjectId:{type:Schema.Types.ObjectId,ref:"subject",select:false},
+   subjectName:{type:String,required:true,select:false},
+   levelName:{type:String,required:true,select:false},
+   levelId:{type:Schema.Types.ObjectId,ref:"Level",select:false},
    content:{type:String,required:true},
    image:{
       public_id:String , secure_url:String
