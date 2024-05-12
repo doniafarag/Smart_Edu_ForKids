49e0c9db7eb3cd56a6e37667e083c1137e8fcbbc
donia <doniafarag390@gmail.com>
2024-05-12 13:59:30 +0300

    uuuuuuu

--- src/modules/auth/auth.controller.js
+++ src/modules/auth/auth.controller.js
@@ -13,227 +13,228 @@ const signUp = catchError(async (req,res,next)=>{
     // console.log(req.protocol);
     // console.log(req.headers.host);
     const {email,password,confirmPassword}=req.body;
-    // let isUser = await userModel.findOne({email})
-    // if(isUser) return next ( AppError.Error('account already exists' ,"failed", 409))
+    let isUser = await userModel.findOne({email})
+    if(isUser) return next ( AppError.Error('account already exists' ,"failed", 409))
     if (password != confirmPassword) return next ( AppError.Error('password not equal confirmpassword',"failed" , 409))
     // const user = new userModel(req.body)
     // await user.save()
     let token = jwt.sign({email},process.env.SECRET_KEY , {expiresIn : 60 * 5})
     let newConfirmEmailToken = jwt.sign({email},process.env.SECRET_KEY , {expiresIn : 60 * 60 * 24 * 30})
-//      const requestNewEmailLink =`${req.protocol}://${req.headers.host}/auth/newConfirmEmailToken/${newConfirmEmailToken}`
-//      const link =`${req.protocol}://${req.headers.host}/auth/confirmEmail/${token}`
-//     // const html = `
-//     //  <a href="${link}"> Confirm Email </a>
-//     //  <br>
-//     //  <br>
-//     //  <a href="${requestNewEmailLink}">Request New confirm email </a>
-//     // `
-//     // =================================================
-//     const html = `
-//     <html>
-
-// <head>
-//   <meta http-equiv="content-type" content="text/html; charset=utf-8">
-//   <meta name="viewport" content="width=device-width, initial-scale=1.0;">
-//   <meta name="format-detection" content="telephone=no" />
-
-//   <style>
-//     /* Reset styles */
-//     body {
-//       margin: 0;
-//       padding: 0;
-//       min-width: 100%;
-//       width: 100% !important;
-//       height: 100% !important;
-//     }
-
-//     body,
-//     table,
-//     td,
-//     div,
-//     p,
-//     a {
-//       -webkit-font-smoothing: antialiased;
-//       text-size-adjust: 100%;
-//       -ms-text-size-adjust: 100%;
-//       -webkit-text-size-adjust: 100%;
-//       line-height: 100%;
-//     }
-
-//     table,
-//     td {
-//       mso-table-lspace: 0pt;
-//       mso-table-rspace: 0pt;
-//       border-collapse: collapse !important;
-//       border-spacing: 0;
-//     }
-
-//     img {
-//       border: 0;
-//       line-height: 100%;
-//       outline: none;
-//       text-decoration: none;
-//       -ms-interpolation-mode: bicubic;
-//     }
-
-//     #outlook a {
-//       padding: 0;
-//     }
-
-//     .ReadMsgBody {
-//       width: 100%;
-//     }
-
-//     .ExternalClass {
-//       width: 100%;
-//     }
-
-//     .ExternalClass,
-//     .ExternalClass p,
-//     .ExternalClass span,
-//     .ExternalClass font,
-//     .ExternalClass td,
-//     .ExternalClass div {
-//       line-height: 100%;
-//     }
-
-//     @media all and (min-width: 560px) {
-//       body {
-//         margin-top: 30px;
-//       }
-//     }
-    
-//     /* Rounded corners */
-//     @media all and (min-width: 560px) {
-//       .container {
-//         border-radius: 8px;
-//         -webkit-border-radius: 8px;
-//         -moz-border-radius: 8px;
-//         -khtml-border-radius: 8px;
-//       }
-//     }
-//     /* Links */
-//     a,
-//     a:hover {
-//       color: #127DB3;
-//     }
-
-//     .footer a,
-//     .footer a:hover {
-//       color: #999999;
-//     }
-//   </style>
-
-//   <!-- MESSAGE SUBJECT -->
-//   <title>Confirm email template</title>
-
-// </head>
-
-// <!-- BODY -->
-// <body topmargin="0" rightmargin="0" bottommargin="0" leftmargin="0" marginwidth="0" marginheight="0" width="100%" style="border-collapse: collapse; border-spacing: 0;  padding: 0; width: 100%; height: 100%; -webkit-font-smoothing: antialiased; text-size-adjust: 100%; -ms-text-size-adjust: 100%; -webkit-text-size-adjust: 100%; line-height: 100%;
-// 	background-color: #F0F0F0;
-// 	color: #000000;" bgcolor="#F0F0F0" text="#000000">
-//   <table width="100%" align="center" border="0" cellpadding="0" cellspacing="0" style="border-collapse: collapse; border-spacing: 0; margin: 0; padding: 0; width: 100%;" class="background">
-//     <tr>
-//       <td align="center" valign="top" style="border-collapse: collapse; border-spacing: 0; margin: 0; padding: 0;" bgcolor="#F0F0F0">
-//         <table border="0" cellpadding="0" cellspacing="0" align="center" bgcolor="#FFFFFF" width="560" style="border-collapse: collapse; border-spacing: 0; padding: 0; width: inherit;
-// 	max-width: 560px;" class="container">
-          
-//           <tr>
-//             <td align="center" valign="top" style="border-collapse: collapse; border-spacing: 0; margin: 0; padding: 0; padding-left: 6.25%; padding-right: 6.25%; width: 87.5%;
-// 			padding-top: 25px;" class="line">
-//               <hr color="#E0E0E0" align="center" width="100%" size="1" noshade style="margin: 0; padding: 0;" />
-//             </td>
-//           </tr>
-//           <tr>
-//             <td align="center" valign="top" style="border-collapse: collapse; border-spacing: 0; margin: 0; padding: 0; padding-left: 6.25%; padding-right: 6.25%; width: 87.5%; font-size: 17px; font-weight: 400; line-height: 160%;
-// 			padding-top: 25px; 
-// 			color: #000000;
-// 			font-family: sans-serif;" class="paragraph">
-//               Hi <span style= color:#3969d5>${req.body.name}</span> ,<br> In order to start using your new account, you need to confirm your email address.
-//             </td>
-//           </tr>
-//           <tr>
-//             <td align="center" valign="top" style="border-collapse: collapse; border-spacing: 0; margin: 0; padding: 0; padding-left: 6.25%; padding-right: 6.25%; width: 87.5%;
-// 			padding-top: 25px;
-// 			padding-bottom: 5px;" class="button">
-//               <a href=${link} target="_blank" style="text-decoration: none;">
-//                 <table border="0" cellpadding="0" cellspacing="0" align="center" style="max-width: 240px; min-width: 120px; border-collapse: collapse; border-spacing: 0; padding: 0;">
-//                   <tr>
-//                     <td align="center" valign="middle" style="padding: 12px 24px; margin: 0; text-decoration: none; border-collapse: collapse; border-spacing: 0; border-radius: 4px; -webkit-border-radius: 4px; -moz-border-radius: 4px; -khtml-border-radius: 4px;"
-//                         bgcolor="#3969d5"><a target="_blank" style="text-decoration: none;
-// 					color: #FFFFFF; font-family: sans-serif; font-size: 17px; font-weight: 400; line-height: 120%;" href=${link}>
-// 						Verify Email Address
-// 					</a>
-//                     </td>
-//                   </tr>
-									 
-//                 </table>
-//               </a>
-//             </td>
-//           </tr>
-// 					 <tr>
-//             <td align="center" valign="top" style="border-collapse: collapse; border-spacing: 0; margin: 0; padding: 0; padding-left: 6.25%; padding-right: 6.25%; width: 87.5%;
-// 			padding-top: 25px;
-// 			padding-bottom: 5px;" class="button">
-//               <a href=${requestNewEmailLink}" target="_blank" style="text-decoration: none;">
-//                 <table border="0" cellpadding="0" cellspacing="0" align="center" style="max-width: 240px; min-width: 120px; border-collapse: collapse; border-spacing: 0; padding: 0;">
-//                   <tr>
-//                     <td align="center" valign="middle" style="padding: 12px 24px; margin: 0; text-decoration: none; border-collapse: collapse; border-spacing: 0; border-radius: 4px; -webkit-border-radius: 4px; -moz-border-radius: 4px; -khtml-border-radius: 4px;"
-//                         bgcolor="#3969d5"><a target="_blank" style="text-decoration: none;
-// 					color: #FFFFFF; font-family: sans-serif; font-size: 17px; font-weight: 400; line-height: 120%;" href=${requestNewEmailLink}>
-// 						Resend New Mail
-// 					</a>
-//                     </td>
-//                   </tr>
-									 
-//                 </table>
-//               </a>
-//             </td>
-//           </tr>
-//           <tr>
-//             <td align="center" valign="top" style="border-collapse: collapse; border-spacing: 0; margin: 0; padding: 0; padding-left: 6.25%; padding-right: 6.25%; width: 87.5%;
-// 			padding-top: 25px;" class="line">
-//               <hr color="#E0E0E0" align="center" width="100%" size="1" noshade style="margin: 0; padding: 0;" />
-//             </td>
-//           </tr>
-//           <tr>
-//             <td align="center" valign="top" style="border-collapse: collapse; border-spacing: 0; margin: 0; padding: 0; padding-left: 6.25%; padding-right: 6.25%; width: 87.5%; font-size: 17px; font-weight: 400; line-height: 160%;
-// 			padding-top: 20px;
-// 			padding-bottom: 25px;
-// 			color: #000000;
-// 			font-family: sans-serif;" class="paragraph">
-//               If you did not sign up for this account you can ignore this email and the account will be deleted.
-//             </td>
-//           </tr>
-//         </table>
-//         <table border="0" cellpadding="0" cellspacing="0" align="center" width="560" style="border-collapse: collapse; border-spacing: 0; padding: 0; width: inherit;
-// 	max-width: 560px;" class="wrapper">
-//           <tr>
-//             <td align="center" valign="top" style="border-collapse: collapse; border-spacing: 0; margin: 0; padding: 0; padding-left: 6.25%; padding-right: 6.25%; width: 87.5%; font-size: 13px; font-weight: 400; line-height: 150%;
-// 			padding-top: 20px;
-// 			padding-bottom: 20px;
-// 			color: #999999;
-// 			font-family: sans-serif;" class="footer">
-//               Check out our extensive FAQ for more information
-//               or contact us through ourContact Form. Our support
-//               team is available to help you 24 hours a day, seven days a week.
-//             </td>
-//           </tr>
-//         </table>
-//       </td>
-//     </tr>
-//   </table>
-// </body>
-// </html>
-//     `;
+     const requestNewEmailLink =`${req.protocol}://${req.headers.host}/auth/newConfirmEmailToken/${newConfirmEmailToken}`
+     const link =`${req.protocol}://${req.headers.host}/auth/confirmEmail/${token}`
+    // const html = `
+    //  <a href="${link}"> Confirm Email </a>
+    //  <br>
+    //  <br>
+    //  <a href="${requestNewEmailLink}">Request New confirm email </a>
+    // `
     // =================================================
-  //  if( !(await sendEmail({to:email , subject:"Confirmation Email" , html}))){
-  //   return next ( AppError.Error('account Rejected' ,"failed", 400))
-  // }
+    const html = `
+    <html>
+
+<head>
+  <meta http-equiv="content-type" content="text/html; charset=utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0;">
+  <meta name="format-detection" content="telephone=no" />
+
+  <style>
+    /* Reset styles */
+    body {
+      margin: 0;
+      padding: 0;
+      min-width: 100%;
+      width: 100% !important;
+      height: 100% !important;
+    }
+
+    body,
+    table,
+    td,
+    div,
+    p,
+    a {
+      -webkit-font-smoothing: antialiased;
+      text-size-adjust: 100%;
+      -ms-text-size-adjust: 100%;
+      -webkit-text-size-adjust: 100%;
+      line-height: 100%;
+    }
+
+    table,
+    td {
+      mso-table-lspace: 0pt;
+      mso-table-rspace: 0pt;
+      border-collapse: collapse !important;
+      border-spacing: 0;
+    }
+
+    img {
+      border: 0;
+      line-height: 100%;
+      outline: none;
+      text-decoration: none;
+      -ms-interpolation-mode: bicubic;
+    }
+
+    #outlook a {
+      padding: 0;
+    }
+
+    .ReadMsgBody {
+      width: 100%;
+    }
+
+    .ExternalClass {
+      width: 100%;
+    }
+
+    .ExternalClass,
+    .ExternalClass p,
+    .ExternalClass span,
+    .ExternalClass font,
+    .ExternalClass td,
+    .ExternalClass div {
+      line-height: 100%;
+    }
+
+    @media all and (min-width: 560px) {
+      body {
+        margin-top: 30px;
+      }
+    }
+    
+    /* Rounded corners */
+    @media all and (min-width: 560px) {
+      .container {
+        border-radius: 8px;
+        -webkit-border-radius: 8px;
+        -moz-border-radius: 8px;
+        -khtml-border-radius: 8px;
+      }
+    }
+    /* Links */
+    a,
+    a:hover {
+      color: #127DB3;
+    }
+
+    .footer a,
+    .footer a:hover {
+      color: #999999;
+    }
+  </style>
+
+  <!-- MESSAGE SUBJECT -->
+  <title>Confirm email template</title>
+
+</head>
+
+<!-- BODY -->
+<body topmargin="0" rightmargin="0" bottommargin="0" leftmargin="0" marginwidth="0" marginheight="0" width="100%" style="border-collapse: collapse; border-spacing: 0;  padding: 0; width: 100%; height: 100%; -webkit-font-smoothing: antialiased; text-size-adjust: 100%; -ms-text-size-adjust: 100%; -webkit-text-size-adjust: 100%; line-height: 100%;
+	background-color: #F0F0F0;
+	color: #000000;" bgcolor="#F0F0F0" text="#000000">
+  <table width="100%" align="center" border="0" cellpadding="0" cellspacing="0" style="border-collapse: collapse; border-spacing: 0; margin: 0; padding: 0; width: 100%;" class="background">
+    <tr>
+      <td align="center" valign="top" style="border-collapse: collapse; border-spacing: 0; margin: 0; padding: 0;" bgcolor="#F0F0F0">
+        <table border="0" cellpadding="0" cellspacing="0" align="center" bgcolor="#FFFFFF" width="560" style="border-collapse: collapse; border-spacing: 0; padding: 0; width: inherit;
+	max-width: 560px;" class="container">
+          
+          <tr>
+            <td align="center" valign="top" style="border-collapse: collapse; border-spacing: 0; margin: 0; padding: 0; padding-left: 6.25%; padding-right: 6.25%; width: 87.5%;
+			padding-top: 25px;" class="line">
+              <hr color="#E0E0E0" align="center" width="100%" size="1" noshade style="margin: 0; padding: 0;" />
+            </td>
+          </tr>
+          <tr>
+            <td align="center" valign="top" style="border-collapse: collapse; border-spacing: 0; margin: 0; padding: 0; padding-left: 6.25%; padding-right: 6.25%; width: 87.5%; font-size: 17px; font-weight: 400; line-height: 160%;
+			padding-top: 25px; 
+			color: #000000;
+			font-family: sans-serif;" class="paragraph">
+              Hi <span style= color:#3969d5>${req.body.name}</span> ,<br> In order to start using your new account, you need to confirm your email address.
+            </td>
+          </tr>
+          <tr>
+            <td align="center" valign="top" style="border-collapse: collapse; border-spacing: 0; margin: 0; padding: 0; padding-left: 6.25%; padding-right: 6.25%; width: 87.5%;
+			padding-top: 25px;
+			padding-bottom: 5px;" class="button">
+              <a href=${link} target="_blank" style="text-decoration: none;">
+                <table border="0" cellpadding="0" cellspacing="0" align="center" style="max-width: 240px; min-width: 120px; border-collapse: collapse; border-spacing: 0; padding: 0;">
+                  <tr>
+                    <td align="center" valign="middle" style="padding: 12px 24px; margin: 0; text-decoration: none; border-collapse: collapse; border-spacing: 0; border-radius: 4px; -webkit-border-radius: 4px; -moz-border-radius: 4px; -khtml-border-radius: 4px;"
+                        bgcolor="#3969d5"><a target="_blank" style="text-decoration: none;
+					color: #FFFFFF; font-family: sans-serif; font-size: 17px; font-weight: 400; line-height: 120%;" href=${link}>
+						Verify Email Address
+					</a>
+                    </td>
+                  </tr>
+									 
+                </table>
+              </a>
+            </td>
+          </tr>
+					 <tr>
+            <td align="center" valign="top" style="border-collapse: collapse; border-spacing: 0; margin: 0; padding: 0; padding-left: 6.25%; padding-right: 6.25%; width: 87.5%;
+			padding-top: 25px;
+			padding-bottom: 5px;" class="button">
+              <a href=${requestNewEmailLink}" target="_blank" style="text-decoration: none;">
+                <table border="0" cellpadding="0" cellspacing="0" align="center" style="max-width: 240px; min-width: 120px; border-collapse: collapse; border-spacing: 0; padding: 0;">
+                  <tr>
+                    <td align="center" valign="middle" style="padding: 12px 24px; margin: 0; text-decoration: none; border-collapse: collapse; border-spacing: 0; border-radius: 4px; -webkit-border-radius: 4px; -moz-border-radius: 4px; -khtml-border-radius: 4px;"
+                        bgcolor="#3969d5"><a target="_blank" style="text-decoration: none;
+					color: #FFFFFF; font-family: sans-serif; font-size: 17px; font-weight: 400; line-height: 120%;" href=${requestNewEmailLink}>
+						Resend New Mail
+					</a>
+                    </td>
+                  </tr>
+									 
+                </table>
+              </a>
+            </td>
+          </tr>
+          <tr>
+            <td align="center" valign="top" style="border-collapse: collapse; border-spacing: 0; margin: 0; padding: 0; padding-left: 6.25%; padding-right: 6.25%; width: 87.5%;
+			padding-top: 25px;" class="line">
+              <hr color="#E0E0E0" align="center" width="100%" size="1" noshade style="margin: 0; padding: 0;" />
+            </td>
+          </tr>
+          <tr>
+            <td align="center" valign="top" style="border-collapse: collapse; border-spacing: 0; margin: 0; padding: 0; padding-left: 6.25%; padding-right: 6.25%; width: 87.5%; font-size: 17px; font-weight: 400; line-height: 160%;
+			padding-top: 20px;
+			padding-bottom: 25px;
+			color: #000000;
+			font-family: sans-serif;" class="paragraph">
+              If you did not sign up for this account you can ignore this email and the account will be deleted.
+            </td>
+          </tr>
+        </table>
+        <table border="0" cellpadding="0" cellspacing="0" align="center" width="560" style="border-collapse: collapse; border-spacing: 0; padding: 0; width: inherit;
+	max-width: 560px;" class="wrapper">
+          <tr>
+            <td align="center" valign="top" style="border-collapse: collapse; border-spacing: 0; margin: 0; padding: 0; padding-left: 6.25%; padding-right: 6.25%; width: 87.5%; font-size: 13px; font-weight: 400; line-height: 150%;
+			padding-top: 20px;
+			padding-bottom: 20px;
+			color: #999999;
+			font-family: sans-serif;" class="footer">
+              Check out our extensive FAQ for more information
+              or contact us through ourContact Form. Our support
+              team is available to help you 24 hours a day, seven days a week.
+            </td>
+          </tr>
+        </table>
+      </td>
+    </tr>
+  </table>
+</body>
+</html>
+    `;
+
+
+   if( !(await sendEmail({to:email , subject:"Confirmation Email" , html}))){
+    return next ( AppError.Error('account Rejected' ,"failed", 400))
+  }
 
   const user = await userModel.create(req.body)
-  res.status(201).json({message: 'success',user,token})
+  return res.status(201).json({message: 'success',user,token})
 })
 
 const confirmEmail = catchError(async (req,res,next)=>{
