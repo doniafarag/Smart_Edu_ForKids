2f62fa38c03e348ec224ff04b6f670be61517413
donia <doniafarag390@gmail.com>
2024-05-25 00:17:18 +0300

    ddddddd

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ import { bootstrap } from './src/bootstrap.js'
 
 dotenv.config()
 const app = express()
-const port = process.env.PORT || 5000
+const port =  5000
 app.use(express.json())
 app.use("/uploads",express.static('./uploads'))
 bootstrap(app)
 // if(process.env.PRODUCTION === "true") return app.listen(port, () => console.log(`production link ${process.env.PRODUCTION_LINK}`))
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(process.env.PORT || port, () => console.log(`Example app listening on port ${port}!`))
