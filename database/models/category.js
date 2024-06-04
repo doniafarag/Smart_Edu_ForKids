31ff08cb53a1a7e19e1200c1f9a2121f7d463c32
donia <doniafarag390@gmail.com>
2024-06-04 22:00:16 +0300

    su

--- database/models/category.js
+++ database/models/category.js
@@ -1,11 +1,11 @@
 import mongoose, { Schema, model } from 'mongoose';
 const catSchema=new Schema({
-   subjectId:{type:Schema.Types.ObjectId,ref:"subject" },
-   subjectName:{type:String },
-   levelId:{type:Schema.Types.ObjectId,ref:"Level"},
-   levelName:{type:String,required:true},
-   unitId:{type:Schema.Types.ObjectId,ref:"Unit"},
-   unitName:{type:String},
+   subjectId:{type:Schema.Types.ObjectId,ref:"subject",select:"false" },
+   subjectName:{type:String,select:"false" },
+   levelId:{type:Schema.Types.ObjectId,ref:"Level",select:"false"},
+   levelName:{type:String,required:true,select:"false"},
+   unitId:{type:Schema.Types.ObjectId,ref:"Unit",select:"false"},
+   unitName:{type:String,select:"false"},
    lessonName:{type:String,required:true},
    text:{type:String},
    image:{
